1d37302cd69ca47a8049e3d3681e0b1bb7585f87
Staniulionis <136055644+Staniulionis@users.noreply.github.com>
2023-06-16 15:59:04 +0300

    Update BasicSimulation.java

--- src/test/java/simulation/BasicSimulation.java
+++ src/test/java/simulation/BasicSimulation.java
@@ -1,30 +1,29 @@
 package simulation;
 
-// 2
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
-public class BasicSimulation extends Simulation { // 3
+public class BasicSimulation extends Simulation {
 
-    HttpProtocolBuilder httpProtocol = http // 4
-            .baseUrl("http://computer-database.gatling.io") // 5
-            .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // 6
+    HttpProtocolBuilder httpProtocol = http
+            .baseUrl("http://computer-database.gatling.io")
+            .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
             .doNotTrackHeader("1")
             .acceptLanguageHeader("en-US,en;q=0.5")
             .acceptEncodingHeader("gzip, deflate")
             .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0");
 
-    ScenarioBuilder scn = scenario("BasicSimulation") // 7
-            .exec(http("request_1") // 8
-                    .get("/")) // 9
-            .pause(5); // 10
+    ScenarioBuilder scn = scenario("BasicSimulation")
+            .exec(http("request_1")
+                    .get("/"))
+            .pause(5);
 
     {
-        setUp( // 11
-                scn.injectOpen(atOnceUsers(1)) // 12
-        ).protocols(httpProtocol); // 13
+        setUp(
+                scn.injectOpen(atOnceUsers(1))
+        ).protocols(httpProtocol);
     }
 }
