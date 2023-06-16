bcb89eabfb4d351b9ccef0fc7140ea5cdc6f326c
Staniulionis <136055644+Staniulionis@users.noreply.github.com>
2023-06-16 16:38:44 +0300

    Update GetSpecificSimulationBooking.java

--- src/test/java/simulation/GetSpecificSimulationBooking.java
+++ src/test/java/simulation/GetSpecificSimulationBooking.java
@@ -11,16 +11,10 @@ import static io.gatling.javaapi.http.HttpDsl.*;
 public class GetSpecificSimulationBooking extends Simulation {
     HttpProtocolBuilder httpProtocol = http
             .baseUrl("http://localhost:3001");
+    
     ScenarioBuilder scn = scenario("GetBookingIds")
             .exec(http("GetSpecificRequest")
                     .get("/booking")
-                    .transformResponse((response, session) -> {
-                                if (response.status().code() == 200) {
-                                    System.out.println(response.body().string());
-                                } else {
-                                    System.out.println(response.body().string());
-                                } return response;
-                            }
                     )
             )
             .pause(5);
